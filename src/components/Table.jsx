68d2b5297d2351741f7fcb6664d4977f6768d48b
Wilson Zheng <wilson.zheng@vacasa.com>
2024-05-18 14:12:04 +1200

    update description

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -14,10 +14,10 @@ import { useQuery } from "react-query";
 import { fetchTotal } from "../queries/fetchTotal.js";
 
 const items = [
-    { id: "A", unitPrice: 50, specialPrice: "3 for 130" },
-    { id: "B", unitPrice: 30, specialPrice: "2 for 45" },
-    { id: "C", unitPrice: 20, specialPrice: null },
-    { id: "D", unitPrice: 15, specialPrice: null },
+    { id: "A", unitPrice: "$0.5", specialPrice: "3 for $1.3" },
+    { id: "B", unitPrice: "$0.3", specialPrice: "2 for $0.45" },
+    { id: "C", unitPrice: "$0.2", specialPrice: null },
+    { id: "D", unitPrice: "$0.15", specialPrice: null },
 ];
 
 const TableComponent = () => {
