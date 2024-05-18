1198ac4cc78ecb08e8559cf57fba1900b589da82
Wilson Zheng <wilson.zheng@vacasa.com>
2024-05-18 12:10:27 +1200

    Configure api url

--- src/queries/fetchTotal.js
+++ src/queries/fetchTotal.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchTotal = async (cartItems) => {
     const response = await axios.get(
-        `http://localhost:5000/v1/checkout?items=${cartItems.join("")}`
+        `${
+            process.env.API_URL ?? "http://localhost:5000"
+        }/v1/checkout?items=${cartItems.join("")}`
     );
     return response.data;
 };
