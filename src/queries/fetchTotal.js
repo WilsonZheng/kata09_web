8279c7e7cc0a928a339eeb18bea6d5ad03e46865
Wilson Zheng <wilson.zheng@vacasa.com>
2024-05-18 12:25:33 +1200

    update api url

--- src/queries/fetchTotal.js
+++ src/queries/fetchTotal.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const fetchTotal = async (cartItems) => {
+    const apiUrl = "https://9b44d6266d61e8ef3295281e24f96751.serveo.net";
+    // http://localhost:5000
     const response = await axios.get(
-        `${
-            process.env.API_URL ?? "http://localhost:5000"
-        }/v1/checkout?items=${cartItems.join("")}`
+        `${apiUrl}/v1/checkout?items=${cartItems.join("")}`
     );
     return response.data;
 };
